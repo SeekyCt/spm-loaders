6b8539a5a34afa748a894596fb467476c6fb3f6a
Seeky <58006653+SeekyCt@users.noreply.github.com>
2022-12-30 13:04:15 +0000

    Add OSFatal.h

---
+++ spm-headers/include/wii/os/OSFatal.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <common.h>
+#include <wii/gx.h>
+
+CPP_WRAPPER(wii::os)
+
+USING(wii::gx::GXColor)
+
+UNKNOWN_FUNCTION(ScreenReport);
+UNKNOWN_FUNCTION(ConfigureVideo);
+void OSFatal(GXColor * fg, GXColor * bg, const char * message);
+UNKNOWN_FUNCTION(Halt);
+
+CPP_WRAPPER_END()
