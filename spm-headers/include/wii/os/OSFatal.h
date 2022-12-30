6a737bcc22988ede141c98ed616746d3452f6170
Seeky <58006653+SeekyCt@users.noreply.github.com>
2022-12-30 14:43:49 +0000

    NORETURN OSFatal

--- spm-headers/include/wii/os/OSFatal.h
+++ spm-headers/include/wii/os/OSFatal.h
@@ -9,7 +9,7 @@ USING(wii::gx::GXColor)
 
 UNKNOWN_FUNCTION(ScreenReport);
 UNKNOWN_FUNCTION(ConfigureVideo);
-void OSFatal(GXColor * fg, GXColor * bg, const char * message);
+void OSFatal(GXColor * fg, GXColor * bg, const char * message) NORETURN;
 UNKNOWN_FUNCTION(Halt);
 
 CPP_WRAPPER_END()
