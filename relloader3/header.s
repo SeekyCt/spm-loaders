b75f1f307d87151f0214e50f8ceeca50d7d2e735
Seeky <58006653+SeekyCt@users.noreply.github.com>
2023-02-18 21:50:21 +0000

    us0 port

--- relloader3/header.s
+++ relloader3/header.s
@@ -12,12 +12,21 @@
 .type header, @object
 header:
 
+// TODO
+#if defined SPM_EU0
+#define HOOK_OFFS 0x6f8
+#elif defined SPM_US0
+#define HOOK_OFFS 0x590
+#else
+.err
+#endif
+
 #define PAYLOAD_MAGIC "RLd3"
 #define PAYLOAD_VERSION 1
 #define CONTEXT loaderCtx
 #define LOAD_ADDRESS 0x80004200
 #define ENTRYPOINT loaderMain
-#define HOOK_ADDRESS (spmarioInit + 0x6f8)
+#define HOOK_ADDRESS (spmarioInit + HOOK_OFFS)
 
 #include "spm_loaders/payloadheader.inc"
 
