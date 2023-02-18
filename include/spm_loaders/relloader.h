27689f32401c3d7c830cd8cc4f1f02a293d77501
Seeky <58006653+SeekyCt@users.noreply.github.com>
2023-02-18 22:51:36 +0000

    Remove context magic

--- include/spm_loaders/relloader.h
+++ include/spm_loaders/relloader.h
@@ -11,8 +11,7 @@ constexpr u32 MAGIC = 0x524c6433; // 'RLd3'
 
 struct RelLoaderContext
 {
-/* 0x0 */ char magic[4]; // RLd3
-/* 0x4 */ void * hostRelContext;
+/* 0x0 */ void * hostRelContext;
 };
 
 typedef spm_loaders::TPayloadHeader<RelLoaderContext, MAGIC> RelLoaderHeader;
